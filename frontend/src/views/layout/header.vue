81f03e55ee29e09994f9b89a44b8f846733e772a
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-04 19:02:13 +0900

    [header] 기본 header 작성

--- frontend/src/views/layout/header.vue
+++ frontend/src/views/layout/header.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    header
+  <div class="header">
+    <v-row no-gutters>
+      <v-col cols="6">
+        <v-img src="@/assets/img/logo_2.png" class="logo"></v-img>
+      </v-col>
+
+      <v-col cols="6">
+        <v-img src="@/assets/img/menu.png" class="menu_icon"></v-img>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -24,3 +32,19 @@ export default {
   }
 }
 </script>
+
+<style>
+.header {
+  height: 5rem;
+}
+.menu_icon{
+  width: 15%;
+  float: right;
+  margin-top: 1.4rem;
+  margin-right: 10px;
+}
+.logo {
+  margin-top: 1.4rem;
+  margin-left: 10px;
+}
+</style>
