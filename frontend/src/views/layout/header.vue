c281290fce4d7f5d0e227726953fae9e8132924c
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-04 19:11:02 +0900

    [header] click 이벤트 추가

--- frontend/src/views/layout/header.vue
+++ frontend/src/views/layout/header.vue
@@ -1,12 +1,16 @@
+<!--
+  name : 이에닮
+  desc : Header
+  date : 2022-01-04
+-->
 <template>
   <div class="header">
     <v-row no-gutters>
       <v-col cols="6">
-        <v-img src="@/assets/img/logo_2.png" class="logo"></v-img>
+        <v-img src="@/assets/img/logo_2.png" class="logo" @click="clickLogo()"></v-img>
       </v-col>
-
       <v-col cols="6">
-        <v-img src="@/assets/img/menu.png" class="menu_icon"></v-img>
+        <v-img src="@/assets/img/menu.png" class="menu_icon" @click="clickMenu()"></v-img>
       </v-col>
     </v-row>
   </div>
@@ -16,35 +20,19 @@
 
 export default {
   name: 'Header',
-  components: {
-  },
-  filters: {
-  },
-  data () {
-    return {
-    }
-  },
-  created () {
-  },
-  mounted () {
-  },
   methods: {
+    clickLogo () {
+      this.$router.push("main")
+    },
+    clickMenu () {
+      console.log('메뉴클릭')
+    }
   }
 }
 </script>
 
 <style>
-.header {
-  height: 5rem;
-}
-.menu_icon{
-  width: 15%;
-  float: right;
-  margin-top: 1.4rem;
-  margin-right: 10px;
-}
-.logo {
-  margin-top: 1.4rem;
-  margin-left: 10px;
-}
+.header {height: 5rem;}
+.logo {margin-top: 1.4rem; margin-left: 10px;}
+.menu_icon{width: 15%; float: right; margin-top: 1.4rem; margin-right: 10px;}
 </style>
