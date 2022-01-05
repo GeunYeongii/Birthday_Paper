5d65f749349a13f030d980979cd6cc7509abc3da
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-05 10:49:52 +0900

    [Vue] icon 플러그인 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
 import vuetify from './plugins/vuetify'
+import { mdiBullhornOutline } from '@mdi/js';
 
 import '@/assets/css/main.css'
 
@@ -12,5 +13,6 @@ new Vue({
   vuetify,
   store,
   router: router,
+  mdiBullhornOutline,
   render: h => h(App)
 }).$mount('#app')
