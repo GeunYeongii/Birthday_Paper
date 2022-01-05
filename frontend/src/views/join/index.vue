2a3eac4be21e2fd4444cc7d2f8d76315dc16a380
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-05 15:04:53 +0900

    [회원가입] 퍼블리싱 완료

--- frontend/src/views/join/index.vue
+++ frontend/src/views/join/index.vue
@@ -1,19 +1,85 @@
 <template>
-  <div>
-    회원가입
-  </div>
+  <v-container>
+    <s-header></s-header>
+    <div class="join">
+
+      <h1>회원가입</h1>
+
+      <v-divider></v-divider>
+
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card ref="form">
+            <v-card-text>
+
+              <form ref="joinForm">
+                <v-row no-gutters>
+                  <v-text-field
+                    label="ID"
+                    :rules="rules"
+                  ></v-text-field>
+                </v-row>
+
+                <v-row no-gutters>
+                  <v-text-field
+                    label="PW"
+                    :rules="rules"
+                  ></v-text-field>
+                </v-row>
+
+                <v-row no-gutters>
+                  <v-text-field
+                    label="PW 체크"
+                    :rules="rules"
+                  ></v-text-field>
+                </v-row>
+
+                <v-row no-gutters>
+                  <v-text-field
+                    label="닉네임"
+                    :rules="rules"
+                  ></v-text-field>
+                </v-row>
+              </form>
+
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+                class="mauto"
+                color="primary"
+                @click="join()"
+                rounded
+                dark
+              >
+                회원가입
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          
+        </v-col>
+      </v-row>
+
+    </div>
+  </v-container>
 </template>
 
 <script>
+import SHeader from '@/views/layout/header'
 
 export default {
   name: 'Join',
   components: {
+    SHeader
   },
   filters: {
   },
   data () {
     return {
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
     }
   },
   created () {
@@ -21,6 +87,9 @@ export default {
   mounted () {
   },
   methods: {
+    join(){
+      console.log('회원가입버튼')
+    }
   }
 }
 </script>
