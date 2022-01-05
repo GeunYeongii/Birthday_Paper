b0ef57ae93bcfa5d8fc6e8b39ded320e27a995ff
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-05 16:17:47 +0900

    [intro] 1차 퍼블리싱 완료

--- frontend/src/views/intro/index.vue
+++ frontend/src/views/intro/index.vue
@@ -1,7 +1,41 @@
+<!--
+  name : 이에닮
+  desc : intro
+  date : 2022-01-05
+-->
 <template>
-  <div>
-    인트로
-  </div>
+  <v-container>
+    <div class="intro">
+      <v-card>
+        <v-card-text>
+          <v-img src="@/assets/img/logo.png"></v-img>
+        </v-card-text>
+
+        <v-card-actions bottom>
+          <div class="mauto">
+            <v-btn
+              color="primary"
+              @click="goLogin()"
+              rounded
+              dark
+            >
+              로그인
+            </v-btn>
+            <v-btn
+              class="ml-4"
+              color="primary"
+              @click="goJoin()"
+              rounded
+              dark
+            >
+              회원가입
+            </v-btn>
+          </div>
+        </v-card-actions>
+      </v-card>
+
+    </div>
+  </v-container>
 </template>
 
 <script>
@@ -21,6 +55,12 @@ export default {
   mounted () {
   },
   methods: {
+    goLogin() {
+      this.$router.push('/login')
+    },
+    goJoin() {
+      this.$router.push('/join')
+    }
   }
 }
 </script>
