eafdd78d038adcb08b46c8421d943aebd5dd77c7
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-05 16:44:09 +0900

    [writeDetail] 1차 퍼블리싱 완료

--- frontend/src/views/write/components/writeDetail.vue
+++ frontend/src/views/write/components/writeDetail.vue
@@ -1,7 +1,39 @@
+<!--
+  name : 이에닮
+  desc : writeDetail
+  date : 2022-01-05
+-->
 <template>
-  <div>
-    WriteDetail
-  </div>
+  <v-dialog
+    v-model="writeDetailDialog"
+    persistent
+    scrollable>
+
+    <v-card id="popupBox" class="popupBox_pcNheightA">
+      <v-card-title class="pop_title_2">
+      </v-card-title>
+      
+        <v-card-text class="conTR_reJ_txtinP">
+          <v-container>
+            <v-row no-gutters>
+              <v-col cols="12">
+                testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions class="pop_boT_btnCon justify-center">
+          <v-btn class="mauto" color="primary" @click="sendMassage()" rounded dark>
+            저장하기
+          </v-btn>
+          <v-btn class="mauto" color="primary" @click="close()" rounded dark>
+            닫기
+          </v-btn>
+        </v-card-actions>
+
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -14,6 +46,7 @@ export default {
   },
   data () {
     return {
+      writeDetailDialog: true
     }
   },
   created () {
@@ -21,6 +54,15 @@ export default {
   mounted () {
   },
   methods: {
+    sendMassage() {
+
+    },
+    open() {
+      this.writeDetailDialog = true
+    },
+    close() {
+      this.writeDetailDialog = false
+    }
   }
 }
 </script>
