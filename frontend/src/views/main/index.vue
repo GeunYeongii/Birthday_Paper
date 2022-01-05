a798f49d20209fc50d955be8648075a34e79525e
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-05 16:17:34 +0900

    [main] 1차 퍼블리싱 완료

--- frontend/src/views/main/index.vue
+++ frontend/src/views/main/index.vue
@@ -1,14 +1,53 @@
+<!--
+  name : 이에닮
+  desc : main
+  date : 2022-01-05
+-->
 <template>
-  <div>
-    카드메인
-  </div>
+  <v-container>
+    <s-header></s-header>
+    <div class="main">
+
+      <h2>n개의 편지가 걸렸어요!</h2>
+
+      <v-divider></v-divider>
+
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card class="main_card">
+            <div class="scroll_bar">  
+              <v-card-text>
+
+              </v-card-text>
+            </div>
+          </v-card>
+          
+          <v-card-actions>
+            <v-btn
+              class="mauto"
+              color="primary"
+              @click="login()"
+              rounded
+              dark
+            >
+              공유하기
+            </v-btn>
+          </v-card-actions>
+          
+        </v-col>
+      </v-row>
+
+    </div>
+  </v-container>
 </template>
 
 <script>
+import SHeader from '@/views/layout/header'
 
 export default {
   name: 'Main',
   components: {
+    SHeader
   },
   filters: {
   },
@@ -24,3 +63,9 @@ export default {
   }
 }
 </script>
+
+<style>
+/* .scroll_bar {
+  max-height: calc(100vh - 230px);
+} */
+</style>
