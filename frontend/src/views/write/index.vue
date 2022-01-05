88b25cb0a0bdf982dce0a67a12ba738131f71e6d
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-05 16:20:09 +0900

    [write] 1차 퍼블리싱 완료

--- frontend/src/views/write/index.vue
+++ frontend/src/views/write/index.vue
@@ -1,14 +1,53 @@
+<!--
+  name : 이에닮
+  desc : write
+  date : 2022-01-05
+-->
 <template>
-  <div>
-    write메인
-  </div>
+  <v-container>
+    <s-header></s-header>
+    <div class="write">
+
+      <h2>000님 에게 편지를 작성해 주세요!</h2>
+
+      <v-divider></v-divider>
+
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-card class="main_card">
+            <div class="scroll_bar">  
+              <v-card-text>
+
+              </v-card-text>
+            </div>
+          </v-card>
+          
+          <v-card-actions>
+            <v-btn
+              class="mauto"
+              color="primary"
+              @click="write()"
+              rounded
+              dark
+            >
+              작성하기
+            </v-btn>
+          </v-card-actions>
+          
+        </v-col>
+      </v-row>
+
+    </div>
+  </v-container>
 </template>
 
 <script>
+import SHeader from '@/views/layout/header'
 
 export default {
   name: 'Write',
   components: {
+    SHeader
   },
   filters: {
   },
@@ -21,6 +60,9 @@ export default {
   mounted () {
   },
   methods: {
+    write() {
+
+    }
   }
 }
 </script>
