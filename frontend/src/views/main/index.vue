101716264c21e4f260e4a07cd03b088e20822f07
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-07 12:53:11 +0900

    [main] 퍼블리싱 완료

--- frontend/src/views/main/index.vue
+++ frontend/src/views/main/index.vue
@@ -8,37 +8,101 @@
     <s-header></s-header>
     <div class="main">
 
-      <h2 class="text-gray">n개의 편지가 걸렸어요!</h2>
+      <h2 class="text-gray">9개의 편지가 도착했어요</h2>
 
       <v-divider></v-divider>
 
-      <v-row justify="center">
+      <v-row justify="center" style="margin-bottom:30px">
         <v-col cols="12">
-          <v-card class="main_card" elevation="3">
-            <div class="scroll_bar">  
-              <v-card-text>
 
-              </v-card-text>
-            </div>
+          <v-card class="mb-4">
+            <v-card-text @click="openDetail()">
+              <span>000님 에게 편지가 도착했어요!</span>
+              <span class="f_rit">
+                <v-icon color="info">{{ icons.mdiCheck }}</v-icon>
+              </span>
+            </v-card-text>
           </v-card>
-          
-          <v-card-actions>
-            <v-btn class="mauto" color="primary" @click="share()" rounded dark elevation="3">
-              공유하기
-            </v-btn>
-          </v-card-actions>
-          
+
+          <v-card class="mb-4">
+            <v-card-text>
+              <span>ㅅㅅㅅ님 에게 편지가 도착했어요!</span>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="mb-4">
+            <v-card-text>
+              <span>ㅁㅁㅁㅁ님 에게 편지가 도착했어요!</span>
+              <span class="f_rit">
+                <v-icon color="info">{{ icons.mdiCheck }}</v-icon>
+              </span>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="mb-4">
+            <v-card-text>
+              <span>ㅇㅇ님 에게 편지가 도착했어요!</span>
+              <span class="f_rit">
+                <v-icon color="info">{{ icons.mdiCheck }}</v-icon>
+              </span>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="mb-4">
+            <v-card-text>
+              <span>ㅇㅇ님 에게 편지가 도착했어요!</span>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="mb-4">
+            <v-card-text>
+              <span>ㅇㅇ님 에게 편지가 도착했어요!</span>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="mb-4">
+            <v-card-text>
+              <span>ㅇㅇ님 에게 편지가 도착했어요!</span>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="mb-4">
+            <v-card-text>
+              <span>ㅇㅇ님 에게 편지가 도착했어요!</span>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="mb-4">
+            <v-card-text>
+              <span>ㅇㅇ님 에게 편지가 도착했어요!!</span>
+            </v-card-text>
+          </v-card>
+
+          <v-card class="mb-4 text-center" elevation="0">
+            <v-card-text>
+              <h5>아직 받은 편지가 없습니다</h5>
+              <h5>친구들 에게 공유해서 편지를 받아보세요!</h5>
+            </v-card-text>
+          </v-card>
+
         </v-col>
       </v-row>
+      <card-detail ref="cardDetail"></card-detail>
+
+      <v-footer class="justify-center pl-0" style="background-color:#fff; padding:0px;" inset app>
+        <v-btn class="mt-3 mb-3" color="primary" dark @click="shareKakao()">
+          공유하기
+        </v-btn>
+      </v-footer> 
 
     </div>
-    <card-detail ref="cardDetail"></card-detail>
   </v-container>
 </template>
 
 <script>
 import SHeader from '@/views/layout/header'
 import cardDetail from '@/views/main/components/cardDetail'
+import { mdiCheck } from '@mdi/js'
 
 export default {
   name: 'Main',
@@ -50,6 +114,9 @@ export default {
   },
   data () {
     return {
+      icons: {
+        mdiCheck
+      },
     }
   },
   created () {
@@ -57,7 +124,10 @@ export default {
   mounted () {
   },
   methods: {
-    share() {
+    openDetail() {
+      this.$refs.cardDetail.open()
+    },
+    shareKakao() {
       
     }
   }
