ff366cfbafd9cf13e23d39642085cbda00edb2b0
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-08 01:31:31 +0900

    [kakaoShare] Kakao 공유하기 test 변수 등록

--- frontend/src/utils/share.js
+++ frontend/src/utils/share.js
@@ -2,8 +2,7 @@ export function kakaoShare() {
   window.Kakao.Link.sendCustom({
     templateId: 68318,
     templateArgs: {
-      'title': '제목 영역입니다.',
-      'description': '설명 영역입니다.'
+      'user':'test'
     }
   });
 }
