44d651a7d1451fa879ea4139cc84435d08e67a8f
taranndus <taranndus@gmail.com>
2022-01-10 15:41:19 +0900

    [Frontend] Add background.png, Cake.png

--- frontend/src/views/intro/index.vue
+++ frontend/src/views/intro/index.vue
@@ -8,10 +8,10 @@
     <v-row no-gutters justify="center" class="mt-16 mb-2">
       <v-col cols="12">
         <v-img width="250" class="mauto mb-6"
-          src="@/assets/img/logo.png"></v-img>
+          src="@/assets/img/cake.png"></v-img>
 
-        <v-img width="210" class="mauto mb-6"
-          src="@/assets/img/gift-box.png"></v-img>
+        <!-- <v-img width="210" class="mauto mb-6"
+          src="@/assets/img/gift-box.png"></v-img> -->
       </v-col>
       
       <v-btn class="mb-2" color="primary" width="90vw" dark @click="goLogin()">
