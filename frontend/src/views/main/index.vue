ddcc952c0c6ecf76829cc7c3cf62f71f2f7f6de7
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-13 01:34:03 +0900

    [main] 반응형 2차 퍼블리싱

--- frontend/src/views/main/index.vue
+++ frontend/src/views/main/index.vue
@@ -15,88 +15,79 @@
         ></v-img>
     </v-col>
     <v-col no-gutters cols="12" class="text-center">
-      <v-card height="50Vh" class="m-auto main_card" elevation="10">
+      <v-card class="m-auto main_card" elevation="10">
 
-        <div class="txtC_474775" v-if="testList.length > 0">
-          <p class="pt-5">♥n개의 편지가 도착했어요♥</p>
+        <div v-if="length > 0">
+          <div class="txtC_474775">
+            <p class="pt-5">♥n개의 편지가 도착했어요♥</p>
+          </div>
+
+          <v-card flat tile>
+            <v-window v-model="onboarding">
+              <v-window-item v-for="n in length" :key="`card-${n}`">
+                <div>
+                  <v-col no-gutters cols="12" class="mt-2 pl-3 pr-3">
+                    <v-row no-gutters>
+                      <v-col class="d-inline-block">
+                        <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_4.png"></v-img>
+                      </v-col>
+                      <v-col class="d-inline-block">
+                        <v-img class="m-auto shadow_eft card_angle mt-3" max-width="90" width="16vw" src="@/assets/img/asset_3.png"></v-img>
+                      </v-col>
+                      <v-col class="d-inline-block">
+                        <v-img class="m-auto shadow_eft" max-width="90" width="16vw" src="@/assets/img/asset_2.png"></v-img>
+                      </v-col>
+                      <v-col class="d-inline-block">
+                        <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_1.png"></v-img>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                  
+                  <v-col no-gutters cols="12" class="mt-2 pl-3 pr-3">
+                    <v-row no-gutters>
+                      <v-col class="d-inline-block">
+                        <v-img class="m-auto shadow_eft mt-1" max-width="90" width="16vw" src="@/assets/img/asset_1.png"></v-img>
+                      </v-col>
+                      <v-col class="d-inline-block">
+                        <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_2.png"></v-img>
+                      </v-col>
+                      <v-col class="d-inline-block">
+                        <v-img class="m-auto shadow_eft mt-1" max-width="90" width="16vw" src="@/assets/img/asset_4.png"></v-img>
+                      </v-col>
+                      <v-col class="d-inline-block">
+                        <v-img class="m-auto shadow_eft card_angle" max-width="90" width="16vw" src="@/assets/img/asset_3.png"></v-img>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                </div>
+
+              </v-window-item>
+            </v-window>
+
+            <v-card-actions class="justify-space-between">
+              <v-btn text @click="prev">
+                <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+              
+              <v-item-group v-model="onboarding" class="text-center" mandatory>
+                <v-item v-for="n in length" :key="`btn-${n}`" v-slot="{ active, toggle }">
+                  <v-btn :input-value="active" icon @click="toggle">
+                    <v-icon>mdi-record</v-icon>
+                  </v-btn>
+                </v-item>
+              </v-item-group>
+
+              <v-btn text @click="next">
+                <v-icon>mdi-chevron-right</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </div>
-
-        <tiny-slider v-bind="tinySliderOptions" v-if="testList.length > 0">
-          <div>
-            <v-col no-gutters cols="12" class="mt-2 pl-3 pr-3">
-              <v-row no-gutters>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_4.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft card_angle mt-3" max-width="90" width="16vw" src="@/assets/img/asset_3.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft" max-width="90" width="16vw" src="@/assets/img/asset_2.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_1.png"></v-img>
-                </v-col>
-              </v-row>
-            </v-col>
-            
-            <v-col no-gutters cols="12" class="mt-2 pl-3 pr-3">
-              <v-row no-gutters>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-1" max-width="90" width="16vw" src="@/assets/img/asset_1.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_2.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-1" max-width="90" width="16vw" src="@/assets/img/asset_4.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft card_angle" max-width="90" width="16vw" src="@/assets/img/asset_3.png"></v-img>
-                </v-col>
-              </v-row>
-            </v-col>
-          </div>
-          <div>
-            <v-col no-gutters cols="12" class="mt-2 pl-3 pr-3">
-              <v-row no-gutters>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_4.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft card_angle mt-3" max-width="90" width="16vw" src="@/assets/img/asset_3.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft" max-width="90" width="16vw" src="@/assets/img/asset_2.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_1.png"></v-img>
-                </v-col>
-              </v-row>
-            </v-col>
-            
-            <v-col no-gutters cols="12" class="mt-2 pl-3 pr-3">
-              <v-row no-gutters>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-1" max-width="90" width="16vw" src="@/assets/img/asset_1.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-3" max-width="90" width="16vw" src="@/assets/img/asset_2.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft mt-1" max-width="90" width="16vw" src="@/assets/img/asset_4.png"></v-img>
-                </v-col>
-                <v-col class="d-inline-block">
-                  <v-img class="m-auto shadow_eft card_angle" max-width="90" width="16vw" src="@/assets/img/asset_3.png"></v-img>
-                </v-col>
-              </v-row>
-            </v-col>
-          </div>
-        </tiny-slider>
 
         <div class="txtC_474775 no_latter" v-else>
           아직 받은 편지가 없습니다.<br>친구들에게 공유해서 편지를 받아보세요!
         </div>
+
       </v-card>
       <v-btn text color="secondary" class="mt-6" @click="shareKakao()">
         공유하기
@@ -125,15 +116,10 @@ export default {
       icons: {
         mdiCheck
       },
-      tinySliderOptions: {
-        mouseDrag: true,
-      },
-      testList: [
-        { "test": "test" },
-        { "test": "test" },
-        { "test": "test" },
-        { "test": "test" },
-      ]
+      // page수
+      length: 5,
+      // 현재 page 위치
+      onboarding: 0,
     }
   },
   created () {
@@ -147,9 +133,24 @@ export default {
     shareKakao() {
       kakaoShare()
     },
+    next () {
+      this.onboarding = this.onboarding + 1 === this.length ? 0 : this.onboarding + 1
+    },
+    prev () {
+      this.onboarding = this.onboarding - 1 < 0 ? this.length - 1 : this.onboarding - 1
+    },
   }
 }
 </script>
 
 <style>
+.v-btn--icon.v-size--default {
+  height: 20px;
+  width: 20px;
+}
+.v-btn--icon.v-size--default .v-icon, .v-btn--fab.v-size--default .v-icon {
+  height: 16px;
+  font-size: 16px;
+  width: 16px;
+}
 </style>
