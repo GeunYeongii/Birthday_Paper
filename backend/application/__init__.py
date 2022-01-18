78d35beaa5115e5b8957964a5f31b30b085dda6c
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-18 19:04:57 +0900

    [Backend] flask jwt 모듈 변경

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from .controller.letterController import letter
 from .controller.userController import user
 import os
@@ -8,9 +9,12 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__)
 CORS(app)
-app.secret_key = 'cky0935'
+# app.secret_key = 'cky0935'
 
 app.register_blueprint(letter)
 app.register_blueprint(user)
+
+app.config.update(JWT_SECRET_KEY = 'cky0935')
+jwt = JWTManager(app)
 
 
