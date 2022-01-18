6f1bb70422810d66b196a2480b30d67ead6aa30c
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-18 15:56:13 +0900

    [userMgmt] DB 연결방식 변경으로 인한 코드 수정

---
+++ backend/application/common/UserMgmt.py
@@ -0,0 +1,27 @@
+from flask_login import UserMixin
+from ..model.dbModule import Database
+
+class UserMgmt(UserMixin):
+
+  @staticmethod
+  def findUserEmail(userEmail):
+    print('== user_info findUserEmail ==')
+    try:
+      db_class = Database()
+      sql = "SELECT * FROM user_info WHERE USER_EMAIL = '" + str(userEmail) + "'"
+      user = db_class.executeOne(sql)
+      return dict(user)
+    except Exception as e:
+      print(e)
+
+  @staticmethod
+  def create(userEmail, userPw, nickName, birth, profileImg):
+    print('== user_info create ==')
+    try:
+      db_class = Database()
+      sql = "INSERT INTO user_info (USER_EMAIL, USER_PW, NICKNAME, BIRTH) VALUES ('%s', '%s', '%s', '%s')" % (
+        str(userEmail), str(userPw), str(nickName), str(birth))
+      db_class.execute(sql)
+      db_class.commit()
+    except Exception as e:
+      print(e)
