3c00fac8563c3c80c28d72592ccee6cdafb8ce14
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-20 02:04:40 +0900

    [backend] actoken 검사 및 rftoken 재발급 함수

--- backend/app/common/JwtService.py
+++ backend/app/common/JwtService.py
@@ -1,26 +1,52 @@
-from flask_jwt_extended import ( jwt_required, create_access_token, get_jwt_identity, create_refresh_token )
+from ..model.UserRepository import UserRepository
+
+from datetime import datetime, timedelta
 from flask import jsonify, request
 from functools import wraps
 
-import datetime
+import jwt, datetime
 
 class JwtService:
   def createAccessToken(userEmail):
-    delta = datetime.timedelta(hours = 1)
-    access_token = create_access_token(identity = userEmail, expires_delta = delta)
+    payload = {'userEmail': userEmail, 'exp': datetime.datetime.utcnow() +timedelta(seconds=5)}
+    access_token = jwt.encode(payload, "SECRET_KEY", algorithm="HS256")
     return access_token
   
   def createRefreshToken(userEmail):
-    delta = datetime.timedelta(days = 14)
-    refresh_token = create_refresh_token(identity = userEmail, expires_delta = delta)
+    payload = {'userEmail': userEmail, 'exp': datetime.datetime.utcnow() +timedelta(days=14)}
+    refresh_token = jwt.encode(payload, "SECRET_KEY", algorithm="HS256")
     return refresh_token
   
+  def checkToken(token):
+    try:
+      decodeToken = jwt.decode(token, "SECRET_KEY", algorithms='HS256')
+      user = UserRepository.findUserByEmail(decodeToken['userEmail'])
+      return user
+    except Exception as e:
+      print(e)
+      return None
+
   # Token 유효성 검사
   def checkJwtRequired(f):
     @wraps(f)
-    @jwt_required()
     def checkJwt(*args, **kwargs):
-      userEmail = get_jwt_identity()
-      return f(userEmail, *args, **kwargs)
+      token = None
+
+      if 'Access-Token' in request.headers:
+        access_token = request.headers['Access-Token']
+        atuser = JwtService.checkToken(access_token)
+      if 'Refresh-Token' in request.headers:
+        refresh_token = request.headers['Refresh-Token']
+        rtuser = JwtService.checkToken(refresh_token)
+
+      if atuser:
+        user = atuser
+      elif rtuser:
+        user = rtuser
+        token = JwtService.createAccessToken(user['USER_EMAIL'])
+      else:
+        return jsonify({'message':'다시 로그인하셈'})
+
+      return f(user, token, *args, **kwargs)
 
     return checkJwt
--- backend/app/controller/letterController.py
+++ backend/app/controller/letterController.py
@@ -9,10 +9,9 @@ letter = Blueprint("letter", __name__, url_prefix="/letter")
 
 @letter.route("/getLetterList", methods=['POST'])
 @JwtService.checkJwtRequired
-def getLetterList(userEmail):
+def getLetterList(user, token=None):
   try:
-    print('userEmail',userEmail)
-
+    print(user, token)
     # [TODO] 데이터 베이스 서버 오픈되면 실 데이터 주고 받는거 테스트 해야함
     # 받은 user idx 값으로 user 데이터 조회
     # user 데이터로 card_data 에서 해당 user가 받은 card list 조회 
