86014f6def5fab5ef1e20e30ade3260a32a8ee3a
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-19 17:54:38 +0900

    [Backend] SQLAlchemy 삭제 => 추후 근영이가 변경할 예정

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from flask_sqlalchemy import SQLAlchemy
 
 from .controller.letterController import letter
 from .controller.userController import user
@@ -12,10 +11,8 @@ CORS(app)
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 app.config['SECRET_KEY'] = 'd9owj3982oi8329dsh38'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://public0917:public2423!@211.47.75.102:3306/dbpublic0917'
 
 app.register_blueprint(letter)
 app.register_blueprint(user)
 
 jwt = JWTManager(app)
-db = SQLAlchemy(app)
